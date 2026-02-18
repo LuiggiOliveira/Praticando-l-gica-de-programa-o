f9eb7b89af10ebcc392841f672a495b24e043088
PhantaLaranja <luiggimcp@gmail.com>
2026-02-17 23:36:14 -0300

    Questão 6 concluída

--- TreinandoLogica/src/lista1/Questao6.java
+++ TreinandoLogica/src/lista1/Questao6.java
@@ -1,4 +1,38 @@
+/*
+Escreva um método estático que receba uma cadeia de caracteres, representada por uma String, que armazena o caminho
+do diretório raiz até um arquivo específico. Exemplo: C:\musicas\somewhereintime\themeofpaganini.mp3 O método deve
+retornar uma nova String contendo apenas o nome do arquivo sem o caminho de diretórios.
+*/
+
 package lista1;
 
+import java.util.Scanner;
+
 public class Questao6 {
+
+    public static String searchString(String root){
+        int i;
+        String resultado = "";
+
+        for (i = root.length() - 1; i >= 0; i--) {
+            if (root.charAt(i) == '\\') { // barra dupla, pois o java acha que é algum comando especial dentro de String
+                break; //guarda a posição da primeira barra da direita para esquerda
+            }
+        }
+
+        for (int j = i+1; j < root.length(); j++) { //começa a partir de onde parou e agora pega só a parte da direita
+            resultado += root.charAt(j); //vai concatenar cada caractere numa variável a qual a função irá retornar
+        }
+
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Cole o caminho do diretório raíz até o arquivo específico: ");
+        String root = input.next(); //C:\musicas\somewhereintime\themeofpaganini.mp3
+        System.out.println(searchString(root));
+    }
 }
--- TreinandoLogica/src/lista1/Questao7.java
+++ TreinandoLogica/src/lista1/Questao7.java
@@ -1,4 +1,11 @@
+/*
+Um número a é dito permutação de um número b se os dígitos de a formam uma permutação dos dígitos de b. Faça um
+programa que lê dois inteiros positivos a e b e responda se a é permutação de b.  Obs.: Considere que o dígito 0 (zero)
+não aparece nos números. Exemplo: 5412434 é uma permutação de 4321445, mas não é uma permutação de 4312455.
+*/
+
 package lista1;
 
 public class Questao7 {
+    public static void main(String[] args) {}
 }
