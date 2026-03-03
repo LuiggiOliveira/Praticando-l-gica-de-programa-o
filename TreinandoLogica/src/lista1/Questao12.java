8c74a1b2c9ef296e4c2227dd804f4bf750cb23f5
PhantaLaranja <luiggimcp@gmail.com>
2026-03-02 23:44:42 -0300

    Questão 12 e 13 concluída

--- TreinandoLogica/src/lista1/Questao12.java
+++ TreinandoLogica/src/lista1/Questao12.java
@@ -1,4 +1,65 @@
+/**
+ * Classe responsável por calcular as somas das séries S1, S2 e S3.
+ * <p>
+ * Faça um programa para calcular o valor das seguintes expressões:
+ * <br>
+ * <img src="doc-files/exercicio_formulas.png" alt="Fórmulas das Séries S1, S2 e S3">
+ * </p>
+ * * @author Luiggi
+ */
+
+//Só testando o javadoc... Ignora. Mas basicamente é para eu calcular as expressões S1, S2, S3 e ver se bate com o resultado...
+//S1 = 1/1 + 3/2 + 5/3 + 7/4 + ... + 99/50 -> resultado: 95,5007946616706
+//S2 = 2¹/50 + 2²/49 + 2³/48 + ... + 2^50/1 -> resultado: 1,5608286920413398E15
+//S3 = 1/1 - 2/4 + 3/9 - 4/16 + 5/25 - ... - 10/100 -> resultado: 0,6456349206349207
+
 package lista1;
 
+//Essa foi fácil, mas gostei
 public class Questao12 {
+    public static void main(String[] args) {
+        //S1 = 1/1 + 3/2 + 5/3 + 7/4 + ... + 99/50 -> resultado: 95,5007946616706
+        double numerador = 1, denominador = 1, resultado = 0; //eu estava colocando como 'int' e isso estava resultando em 50
+        while (denominador <= 50) {
+            resultado +=  numerador / denominador;
+            numerador += 2;
+            denominador++;
+        }
+        System.out.println("Resultado de S1: " + resultado);
+
+        //S2 = 2¹/50 + 2²/49 + 2³/48 + ... + 2^50/1 -> resultado: 1,5608286920413398E15
+        numerador = 2;
+        denominador = 50;
+        resultado = 0;
+        while (denominador >= 1) {
+            resultado +=  numerador / denominador;
+            numerador *= 2;
+            denominador--;
+        }
+        System.out.println("Resultado de S2: " + resultado);
+
+        //S3 = 1/1 - 2/4 + 3/9 - 4/16 + 5/25 - ... - 10/100 -> resultado: 0,6456349206349207
+        //num. impar par impar   par    impar        par, ou seja, de impar -> par é -, de par -> impar é +
+        numerador = 1;
+        denominador = 1;
+        resultado = 0;
+        int contador = 1;
+
+        while (denominador <= 100) {
+            //sempre que o contador for par, subtraio de resultado
+            if (contador % 2 == 0) {
+                resultado -=  numerador / denominador;
+            }
+
+            //caso contrário, quando é par, soma normalmente
+            else {
+                resultado += numerador / denominador;
+            }
+
+            numerador++;
+            denominador = Math.pow(numerador, 2); //já que o denominador é o quadrado do numerador
+            contador++;
+        }
+        System.out.println("Resultado de S3: " + resultado);
+    }
 }
--- TreinandoLogica/src/lista1/Questao13.java
+++ TreinandoLogica/src/lista1/Questao13.java
@@ -1,4 +1,44 @@
+/*
+Observe a seguinte propriedade que alguns números maiores que 1000 e menores que 9999 possuem:
+Número: abcd
+        (ab) + (cd) = (ef)
+        (ef)² = abcd
+Exemplo: 3025
+         30 + 25 = 55
+         55² = 3025
+Resultado esperado: 2025 3025 9801
+
+Faça um programa que imprima todos os números que satisfaçam esta propriedade.
+ */
+
 package lista1;
 
 public class Questao13 {
+
+    //preciso dividir pela metade o número, já que está entre 1000 e 9999, ou seja,
+    //sempre é um número de 4 dígitos
+
+    //separo em duas variáveis e depois somo elas em um resultado
+
+    //pego esse resultado e elevo ao quadro, depois verifico se é igual ao valor de entrada
+
+    public static void verificar(){
+        int a, b, soma;
+        for (int i = 1000; i <= 9999; i++) {
+            a = i%100; //3025%100 = 25
+            b = i/100; //3025/100 = 30
+            soma = a+b;
+            soma = (int) Math.pow(soma, 2);
+
+            if (soma == i) {
+                System.out.println(soma);
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        verificar();
+    }
 }
